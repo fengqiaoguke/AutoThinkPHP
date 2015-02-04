3d5350c70a1cf822b38727fc2745e5625b57164d
枫桥孤客 <wh1232005@21cn.com>
2015-02-04 17:48:14 +0800

    修改配置文件

--- Common/Conf/config.php
+++ Common/Conf/config.php
@@ -3,10 +3,10 @@ return array(
     
     // 数据库配置
     'DB_TYPE' => 'mysql', // 数据库类型
-    'DB_HOST' => 'rdsqvr322vabjbr.mysql.rds.aliyuncs.com', // 服务器地址
-    'DB_NAME' => 'allergan', // 数据库名
-    'DB_USER' => 'ata_rw', // 用户名
-    'DB_PWD' => 'ata2012', // 密码
+    'DB_HOST' => 'localhost', // 服务器地址
+    'DB_NAME' => 'test', // 数据库名
+    'DB_USER' => 'root', // 用户名
+    'DB_PWD' => '', // 密码
     'DB_PORT' => 3306, // 端口
     'DB_PREFIX' => 'kj_'
 );
