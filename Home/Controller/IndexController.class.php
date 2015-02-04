40669fc3cb38066a0bbe3080eff8332b0e8dfa52
枫桥孤客 <wh1232005@21cn.com>
2015-02-04 17:42:32 +0800

    restful

--- Home/Controller/IndexController.class.php
+++ Home/Controller/IndexController.class.php
@@ -1,13 +1,36 @@
 <?php
 namespace Home\Controller;
+
 use Think\Controller;
 use Home\Model\ToolModel;
-use Home\Model\UserModel;
-use Home\Model\Fqj_userModel;
-class IndexController extends Controller {
+ 
+class IndexController extends Controller
+{
+
+    public function _initialize()
+    {
+        $fun = '_'.strtolower(REQUEST_METHOD);
+        $this->$fun();
+    }
+
+    public function index()
+    {
+        $tool = new ToolModel();
+        // $tool->run();
     
-  public function index(){
-      $tool = new ToolModel();
-      $tool->run();
-  }
+    }
+    public function _get(){
+        echo 'get';
+    }
+    
+    public function _post(){
+        echo 'post';
+    }
+    
+    public function _put(){
+        echo 'put';
+    }
+    public function _delete(){
+        echo 'delete';
+    }
 }
